8a2a25691018d73dee6105bd0594e1371887ed96
thr3a <thr3a@example.com>
2026-02-23 18:58:40 +0900

    fix: lint

--- src/remark-plugins/removeBold.ts
+++ src/remark-plugins/removeBold.ts
@@ -1,21 +1,11 @@
+import type { Root } from 'mdast';
 import { visit } from 'unist-util-visit';
 
-type NodeWithChildren = {
-  children: unknown[];
-};
-
-const hasChildren = (value: unknown): value is NodeWithChildren => {
-  if (typeof value !== 'object' || value === null) return false;
-  if (!('children' in value)) return false;
-  return Array.isArray(value.children);
-};
-
 export const remarkRemoveBold = () => {
-  return (tree: { type: string }) => {
+  return (tree: Root) => {
     visit(tree, 'strong', (node, index, parent) => {
       if (typeof index !== 'number') return;
-      if (!hasChildren(parent)) return;
-      if (!hasChildren(node)) return;
+      if (!parent) return;
 
       parent.children.splice(index, 1, ...node.children);
     });
